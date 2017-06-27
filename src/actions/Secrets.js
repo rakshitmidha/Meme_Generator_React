cb158da626a22c663a127705c965b7b4890a2544
Rakshit <rakshit@infrrd.ai>
2017-06-28 03:08:39 +0530

    Adding Redux-thunk and applying middleware

---
+++ src/actions/Secrets.js
@@ -0,0 +1,4 @@
+const username = 'rakshit1008';
+const password = 'Rakshit1008!';
+
+export { username, password };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { fetchMemes } from './actions';
+
+const store = createStore( rootReducer, applyMiddleware(thunk) );
+
+store.subscribe( () => console.log('store', store.getState()));
+store.dispatch( fetchMemes() );
+ReactDOM.render(
+  <Provider store = { store }>
+  <App />
+  </Provider>,
+  document.getElementById('root')
+);
