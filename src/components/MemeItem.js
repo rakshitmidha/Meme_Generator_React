975c7b2bbab9525b2e5f21ea08628cb61411dfe5
Rakshit <rakshit@infrrd.ai>
2017-06-28 14:30:03 +0530

    Displaying API result on the App and adding styles & animations

---
+++ src/components/MemeItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class MemeItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hovered : false
+    }
+  }
+  render() {
+    return (
+      <div
+          className = "meme-item"
+          onMouseEnter = {() => this.setState({hovered : true})}
+          onMouseLeave = {() => this.setState({hovered : false})}>
+        <img src= {this.props.meme.url}
+             alt= {this.props.meme.name}
+             className = {this.state.hovered ? "meme-image darken-img" : "meme-image"}
+           />
+        <p className= {this.state.hovered ? "meme-text" : "no-text"}>
+          {this.props.meme.name}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default MemeItem;
